6b827df8f7e4b6e0f553852f6060ba235fd25b1b
Heidi641 <heidi.kimari@gmail.com>
2026-08-22 16:52:10 +0300

    Update staging test account emails

--- src/lib/staging.ts
+++ src/lib/staging.ts
@@ -3,7 +3,7 @@ export const isStaging = process.env.NEXT_PUBLIC_APP_ENV === "staging";
 export const stagingLabel = "TESTIYMPÄRISTÖ - EI OIKEITA KAUPPOJA";
 
 export const testAccounts = [
-  { role: "Kuluttaja", email: "kuluttaja.testi@joukko.local", password: "JoukkoTesti2026!" },
-  { role: "Yritys", email: "yritys.testi@joukko.local", password: "JoukkoTesti2026!" },
-  { role: "Admin", email: "admin.testi@joukko.local", password: "JoukkoTesti2026!" }
+  { role: "Kuluttaja", email: "kuluttaja.testi@joukko.fi", password: "JoukkoTesti2026!" },
+  { role: "Yritys", email: "yritys.testi@joukko.fi", password: "JoukkoTesti2026!" },
+  { role: "Admin", email: "admin.testi@joukko.fi", password: "JoukkoTesti2026!" }
 ];
